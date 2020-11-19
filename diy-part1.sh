3a355b2d159fcab99301d808e18d6fb84532d7c8
Black0Bag <1216300859@qq.com>
2020-11-19 13:46:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,9 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-git clone https://github.com/kenzok8/small package/kenzol8/small
-git clone https://github.com/kenzok8/openwrt-packages package/kenzol8/package
 git clone https://github.com/Lienol/openwrt-package package/lienol
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
